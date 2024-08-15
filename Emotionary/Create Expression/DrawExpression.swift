5bf30bd8a6b107f22f1570db35188b2577b29ae1
pravshot <praveenkalva@icloud.com>
2024-08-15 13:54:43 -0500

    fixed drawing canvas updates bug

--- Emotionary/Create Expression/DrawExpression.swift
+++ Emotionary/Create Expression/DrawExpression.swift
@@ -10,25 +10,62 @@ import PencilKit
 
 struct DrawExpression: View {
     @State var prompt: String
-    @State var canvas = PKCanvasView()
     var isTodaysExpression: Bool
-    @State var showOverlay = false
     @State var refreshView = false
+    
+    @State var canvas = PKCanvasView()
+    @State var color: Color = .accentColor
+    @State var drawingTool: PKInkingTool.InkType = .pen
+    @State var isDrawing = true
+    @Environment(\.undoManager) var undoManager
 
     var body: some View {
         VStack {
             ExpressionPrompt(prompt: $prompt)
                 .padding()
                 .id(refreshView)
-            DrawingCanvas(canvas: $canvas)
-        }
-        .overlay {
-            if showOverlay {
-                Image(uiImage: canvas.drawing.image(from: canvas.drawing.bounds, scale: 1.0))
-                    .resizable()
-                    .scaledToFit()
-                    .border(.black)
-            }
+            CanvasView(canvas: $canvas, 
+                       color: $color,
+                       drawingTool: $drawingTool,
+                       isDrawing: $isDrawing,
+                       onChange: {refreshView.toggle()}
+            )
+                .toolbar {
+                    ToolbarItemGroup(placement: .bottomBar) {
+                        Spacer()
+                        Button {
+                            undoManager?.undo()
+                        } label: {
+                            Image(systemName: "arrow.uturn.backward")
+                        }
+                        .disabled((undoManager != nil) && !undoManager!.canUndo)
+                        Button {
+                            undoManager?.redo()
+                        } label: {
+                            Image(systemName: "arrow.uturn.forward")
+                        }
+                        .disabled((undoManager != nil) && !undoManager!.canRedo)
+                        Button {
+                            drawingTool = .pen
+                            isDrawing = true
+                        } label: {
+                            Image(systemName: (drawingTool == .pen && isDrawing)  ? "pencil.line" : "pencil")
+                        }
+                        Button {
+                            drawingTool = .marker
+                            isDrawing = true
+                        } label: {
+                            Image(systemName: (drawingTool == .marker && isDrawing) ? "paintbrush.pointed.fill" : "paintbrush.pointed")
+                        }
+                        Button {
+                            isDrawing = false
+                        } label: {
+                            Image(systemName: isDrawing ? "eraser" : "eraser.fill")
+                        }
+                        ColorPicker("", selection: $color)
+                    }
+                }
+                .frame(height: nil)
         }
         .toolbar {
             ToolbarItemGroup {
@@ -36,9 +73,6 @@ struct DrawExpression: View {
                     ExpressionForm(drawing: getUIImage(canvas), prompt: prompt, isTodaysExpression: isTodaysExpression)
                 }
                 .disabled(isCanvasEmpty(canvas))
-                Button("overlay") {
-                    showOverlay.toggle()
-                }
             }
         }
         .navigationTitle("New Expression")
@@ -52,6 +86,59 @@ func isCanvasEmpty(_ canvas: PKCanvasView) -> Bool {
     return canvas.drawing.bounds.isEmpty
 }
 
+struct CanvasView: UIViewRepresentable {
+    @Binding var canvas: PKCanvasView
+    @Binding var color: Color
+    @Binding var drawingTool: PKInkingTool.InkType
+    @Binding var isDrawing: Bool
+    
+    let onChange: () -> Void
+    
+    var ink: PKInkingTool {
+        PKInkingTool(drawingTool, color: UIColor(color))
+    }
+    
+    let eraser = PKEraserTool(.bitmap)
+    
+    func makeUIView(context: Context) -> PKCanvasView {
+        canvas.drawingPolicy = .anyInput
+        canvas.tool = isDrawing ? ink : eraser
+        canvas.minimumZoomScale = 1.0
+        canvas.maximumZoomScale = 2.5
+        
+        let toolPicker = PKToolPicker.init()
+        toolPicker.setVisible(true, forFirstResponder: canvas)
+        toolPicker.addObserver(canvas)
+        canvas.becomeFirstResponder()
+        
+        canvas.delegate = context.coordinator
+
+        return canvas
+    }
+    
+    func updateUIView(_ uiView: PKCanvasView, context: Context) {
+        uiView.tool = isDrawing ? ink : eraser
+    }
+    
+    func makeCoordinator() -> Coordinator {
+      Coordinator(canvasView: $canvas, onChange: onChange)
+    }
+}
+
+class Coordinator: NSObject, PKCanvasViewDelegate {
+    var canvasView: Binding<PKCanvasView>
+    let onChange: () -> Void
+
+    init(canvasView: Binding<PKCanvasView>, onChange: @escaping () -> Void) {
+        self.canvasView = canvasView
+        self.onChange = onChange
+    }
+
+    func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
+        onChange()
+    }
+}
+
 extension UIWindow {
     static var current: UIWindow? {
         for scene in UIApplication.shared.connectedScenes {
@@ -63,8 +150,6 @@ extension UIWindow {
         return nil
     }
 }
-
-
 extension UIScreen {
     static var current: UIScreen? {
         UIWindow.current?.screen
