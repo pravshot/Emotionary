27f271da4cfcf47e0109aa22e0772cce4d6f98bb
pravshot <praveenkalva@icloud.com>
2024-09-04 13:34:28 -0500

    daily notification reminder

--- Emotionary/ContentView.swift
+++ Emotionary/ContentView.swift
@@ -7,9 +7,9 @@
 
 import SwiftUI
 import SwiftData
+import UserNotifications
 
 struct ContentView: View {
-
     @State var tab: Tab = .home
 
     var body: some View {
@@ -27,8 +27,49 @@ struct ContentView: View {
                 }
                 .tag(Tab.gallery)
         }
+        .onAppear {
+            requestNotificationPermission()
+            setupNotification()
+        }
     }
+}
 
+func requestNotificationPermission() {
+    let center = UNUserNotificationCenter.current()
+    center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+        if granted {
+            print("Permission granted")
+        } else {
+            print("Permission denied")
+        }
+    }
+}
+
+func setupNotification() {
+    cancelDailyNotification()
+    scheduleDailyNotification()
+}
+
+func scheduleDailyNotification() {
+    // Notification Content
+    let content = UNMutableNotificationContent()
+    content.title = "Create an Expression"
+    content.body = "Take some time to reflect and make an expression!"
+    content.sound = UNNotificationSound.default
+    // Notification Timing
+    let dateComponents = DateComponents(hour: 20, minute: 0) // 8 pm daily
+    let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+    let request = UNNotificationRequest(identifier: "DailyReminder", content: content, trigger: trigger)
+    // Send Notification
+    UNUserNotificationCenter.current().add(request) { error in
+        if let error = error {
+            print("Error scheduling notification: \(error)")
+        }
+    }
+}
+
+func cancelDailyNotification() {
+    UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: ["DailyReminder"])
 }
 
 enum Tab {
