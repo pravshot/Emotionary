235447e5acbc2e7743f046c76458bc5933c05476
pravshot <praveenkalva@icloud.com>
2024-11-27 00:06:42 -0600

    implemented delete logic with feed updating

--- Expressly/Gallery Views/ExpressionFeedItem.swift
+++ Expressly/Gallery Views/ExpressionFeedItem.swift
@@ -35,8 +35,7 @@ struct ExpressionFeedItem: View {
                     }
                     .confirmationDialog("Are you sure you want to delete this expression?", isPresented: $isPresentingConfirm) {
                         Button("Delete Expression", role: .destructive) {
-                            // need to implement deletion logic
-                            // modelContext.delete(expression)
+                             modelContext.delete(expression)
                         }
                     } message: {
                         Text("You cannot undo this action")
