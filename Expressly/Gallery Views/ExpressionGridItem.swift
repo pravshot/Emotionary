5679d643147b9c11f08979755326a92837aeb718
pravshot <praveenkalva@icloud.com>
2025-02-06 22:04:01 -0600

    cropped scaling in grid items

--- Expressly/Gallery Views/ExpressionGridItem.swift
+++ Expressly/Gallery Views/ExpressionGridItem.swift
@@ -11,35 +11,38 @@ struct ExpressionGridItem: View {
     var expression: Expression
     
     var body: some View {
-        Image(uiImage: expression.getUIImage())
-            .resizable()
-            .aspectRatio(1, contentMode: .fit)
-            .clipShape(.rect(cornerRadius: 8))
-            .padding()
-            .overlay {
-                RoundedRectangle(cornerRadius: 8)
-                    .inset(by: 0.5)
-                    .stroke(.gray.opacity(0.2), lineWidth: 1)
-            }
-            .overlay(alignment: .bottomTrailing) {
-                Image(expression.emotion!.icon)
-                    .resizable()
-                    .frame(width: 32, height: 32)
-                    .offset(x: -2, y: -2)
-            }
-            .contextMenu {
-                ShareLink(
-                    item: Image(uiImage: expression.getUIImage()),
-                    subject: Text(expression.title),
-                    message: Text(expression.caption),
-                    preview: SharePreview(
-                        expression.title,
-                        image: Image(uiImage: expression.getUIImage())
-                    )
-                ) {
-                    Label("Share", systemImage: "square.and.arrow.up")
+        GeometryReader { geometry in
+            Image(uiImage: expression.getUIImage())
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: geometry.size.width, height: geometry.size.width)
+                .clipped()
+                .clipShape(.rect(cornerRadius: 8))
+                .overlay {
+                    RoundedRectangle(cornerRadius: 8)
+                        .inset(by: 0.5)
+                        .stroke(.gray.opacity(0.2), lineWidth: 1)
                 }
-            }
+                .overlay(alignment: .bottomTrailing) {
+                    Image(expression.emotion!.icon)
+                        .resizable()
+                        .frame(width: 32, height: 32)
+                        .offset(x: -2, y: -2)
+                }
+                .contextMenu {
+                    ShareLink(
+                        item: Image(uiImage: expression.getUIImage()),
+                        subject: Text(expression.title),
+                        message: Text(expression.caption),
+                        preview: SharePreview(
+                            expression.title,
+                            image: Image(uiImage: expression.getUIImage())
+                        )
+                    ) {
+                        Label("Share", systemImage: "square.and.arrow.up")
+                    }
+                }
+        }.aspectRatio(1, contentMode: .fit)
     }
 }
 
