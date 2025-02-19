94ec69497a7e2d7c000e83c7b31800441d113286
pravshot <praveenkalva@icloud.com>
2025-02-19 00:16:34 -0600

    expression feed button relayout

--- Expressly/Gallery Views/ExpressionFeedItem.swift
+++ Expressly/Gallery Views/ExpressionFeedItem.swift
@@ -18,31 +18,26 @@ struct ExpressionFeedItem: View {
             VStack(alignment: .leading) {
                 GroupBox {
                     HStack {
+                        Image(expression.emotion!.icon)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 28)
+                            .padding(.trailing, 3)
                         Text(expression.prompt)
                             .font(.subheadline)
-                            .foregroundStyle(.gray)
+                            .fontWeight(.medium)
                             .fixedSize(horizontal: false, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
                         Spacer()
-                        Menu {
-                            Button(role: .destructive) {
-                                isPresentingConfirm = true
-                            } label: {
-                                Label("Delete", systemImage: "trash.fill")
-                            }
-                        } label: {
-                            Image(systemName: "ellipsis.circle")
-                                .foregroundStyle(.accent)
-                        }
-                    }
-                    .confirmationDialog("Are you sure you want to delete this expression?", isPresented: $isPresentingConfirm) {
-                        Button("Delete Expression", role: .destructive) {
-                             modelContext.delete(expression)
-                        }
-                    } message: {
-                        Text("You cannot undo this action")
                     }
                 }
-                .backgroundStyle(colorScheme == .light ? .white : Color(UIColor.secondarySystemGroupedBackground))
+                .padding(.horizontal, -4)
+                .backgroundStyle(
+                    LinearGradient(
+                            gradient: Gradient(colors: [expression.emotion!.color.opacity(0.1), expression.emotion!.color.opacity(0.25)]),
+                            startPoint: .leading,
+                            endPoint: .trailing
+                        )
+                )
                 GroupBox {
                     ZStack(alignment: .bottomTrailing) {
                         HStack {
@@ -55,7 +50,7 @@ struct ExpressionFeedItem: View {
                                 })
                             Spacer()
                         }
-                        HStack(spacing: 12) {
+                        HStack(spacing: 16) {
                             Button {
                                 expression.favorite.toggle()
                             } label: {
@@ -80,13 +75,9 @@ struct ExpressionFeedItem: View {
                                     .aspectRatio(contentMode: .fit)
                                     .frame(height: 25)
                             }
-                            Image(expression.emotion!.icon)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(height: 26)
                         }
                         .fixedSize()
-                        .padding(.horizontal, 10)
+                        .padding(.horizontal, 12)
                         .padding(.vertical, 10)
                         .background(
                             Capsule()
@@ -100,50 +91,59 @@ struct ExpressionFeedItem: View {
                         .offset(x:10, y:10)
                     }
                 }
+                .padding(.horizontal, -4)
                 .backgroundStyle(colorScheme == .dark ? .black : .white)
                 if !expression.caption.isEmpty {
-                    Text(expression.caption)
-                        .font(.body)
+                    ScrollView {
+                        Text(expression.caption)
+                            .font(.body)
+                            .lineLimit(nil)
+                    }
+                    .frame(maxHeight: 80)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .padding(.vertical, 8)
+                    .padding(.horizontal, 8)
+                    Divider()
                 }
                 HStack(alignment: .bottom) {
-                    Spacer()
                     Text(formatDate(expression.date))
-                        .font(.headline)
-                        .fontWeight(.bold)
-                        .foregroundStyle(Color(UIColor.lightGray))
+                        .font(.callout)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.gray)
                     Spacer()
-                    
+                    Menu {
+                        Button(role: .destructive) {
+                            isPresentingConfirm = true
+                        } label: {
+                            Label("Delete", systemImage: "trash.fill")
+                        }
+                    } label: {
+                        Image(systemName: "ellipsis.circle")
+                            .resizable()
+                            .foregroundStyle(.accent)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height: 20)
+                    }
                 }
-                .padding(.top, 8)
+                .padding(.top, 4)
+                .padding(.horizontal, 8)
+                .confirmationDialog("Are you sure you want to delete this expression?", isPresented: $isPresentingConfirm) {
+                    Button("Delete Expression", role: .destructive) {
+                         modelContext.delete(expression)
+                    }
+                } message: {
+                    Text("You cannot undo this action")
+                }
             }
             .frame(maxWidth: 600, maxHeight: getMaxPostHeight())
         }
-        .backgroundStyle(
-            RadialGradient(gradient: Gradient(colors: [.clear, expression.emotion!.color.opacity(0.35)]),
-                           center: .center,
-                           startRadius: 50,
-                           endRadius: 750
-            )
-        )
     }
 }
 
 func formatDate(_ date: Date) -> String {
-    let calendar = Calendar.current
     let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "MMMM d"
-    let day = dateFormatter.string(from: date)
-    let timeOfDay = switch calendar.component(.hour, from: date) {
-        case 5..<12:
-            "Morning"
-        case 12..<17:
-            "Afternoon"
-        case 17..<21:
-            "Evening"
-        default:
-            "Night"
-    }
-    return (day + " " + timeOfDay).uppercased()
+    dateFormatter.dateFormat = "EEEE, MMM d"
+    return dateFormatter.string(from: date)
 }
 
 func getMaxPostHeight() -> CGFloat? {
